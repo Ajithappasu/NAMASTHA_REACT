f5199ccff1c4eac133a24a6d4d469e22488e2abd
Ajith Sai Appasu <ajithappasu@gmail.com>
2026-02-05 13:29:36 +0530

    react components

--- App.js
+++ App.js
@@ -1,24 +1,30 @@
 import React  from "react";
 import ReactDOM from "react-dom/client";
-// this object is helpful in giving attributes  to the element id, etc
-    /// ("tagName", {id:"myId"}, content)
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-    // each child element should have a uniques key componenet
-    React.createElement(
-      "div",
-      { id: "child", key: "child1" },
-      React.createElement("h1", {}, "hello from the child 1")
-    ),
-    React.createElement(
-      "div",
-      { id: "child", key: "child2" },
-      React.createElement("h1", {}, "hello from the child 2")
-    )
-  ]
-);
 
+const heading = React.createElement("h1",{id :"heading"},"hello from react create element");
+
+console.log(heading);
+// if we want to write JSX in multiple lines we have to wrap it in  paratasis ()
+// const jsxHeading = (<h1 id ="heading"  className="Main">
+//     hello from jsx 
+//     </h1>);
+
+
+// this can also be written without return 
+ const FunctionalHeading =()=>(
+    <div id="container">
+        <h1>heloo without return </h1>
+        </div>);
+
+// react functional component 
+ const HeadingFunctional = ()=>{
+    return(<div> 
+        <FunctionalHeading  />
+        <h1>hello from react function</h1>
+        </div>);
+ };
+ // this is also callled   component compozision
+ 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// we have to vrap it in div to render 
+root.render(<HeadingFunctional/>);
