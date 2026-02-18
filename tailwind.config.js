7734010ce92023c3d3914751d608032a6fd3231e
Ajith Sai Appasu <ajithappasu@gmail.com>
2026-02-18 14:03:53 +0530

    error setting

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"], // we are teling the tailwind that we can use tailwind config in all type of files in src folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
